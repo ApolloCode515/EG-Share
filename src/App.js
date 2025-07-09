f6ddfa981d0b8b52ced4d4ea15abebe9251868ae
crystalr01 <crystalr01@gmail.com>
2025-07-09 12:45:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SharePage from './SharePage'; // Import the new component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SharePage />  {/* Render the SharePage component */}
     </div>
   );
 }
